d2f2105c63be9543ac60e51a47a63c86e6f39cb5
Gabriel Duarte <gabriellagoa10@yahoo.com.br>
2017-07-21 22:25:49 -0300

    Requisição de camera e fechar a tela após ler.

--- app/src/main/java/duarte/gabriel/maquina/MainActivity.java
+++ app/src/main/java/duarte/gabriel/maquina/MainActivity.java
@@ -1,11 +1,17 @@
 package duarte.gabriel.maquina;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 import com.google.zxing.Result;
@@ -26,7 +32,12 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         btnAtivarLeitor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                QrScanner(v);
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_DENIED)
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CAMERA}, CODE);
+                else if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                        == PackageManager.PERMISSION_GRANTED)
+                    QrScanner();
             }
         });
     }
@@ -34,17 +45,15 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
     @Override
     public void onPause() {
         super.onPause();
-        mScannerView.stopCamera();           // Stop camera on pause
+        //mScannerView.stopCamera();
     }
 
-    public void QrScanner(View view){
+    public void QrScanner(){
 
-
-        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+        mScannerView = new ZXingScannerView(this);
         setContentView(mScannerView);
-
-        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
-        mScannerView.startCamera();         // Start camera
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
 
     }
 
@@ -54,16 +63,20 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         Log.e("handler", rawResult.getText()); // Prints scan results
         Log.e("handler", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode)
 
-        // show the scanner result into dialog box.
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Scan Result");
-        builder.setMessage(rawResult.getText());
-        AlertDialog alert1 = builder.create();
-        alert1.show();
-
-        // If you would like to resume scanning, call this method below:
-        // mScannerView.resumeCameraPreview(this);
+        Toast.makeText(this, "QR: " + rawResult.getText(), Toast.LENGTH_LONG).show();
+        mScannerView.stopCamera();
+        mScannerView.resumeCameraPreview(this);
+        setContentView(R.layout.activity_main);
     }
 
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == CODE){
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED)
+                Toast.makeText(this, "Você precisa fornecer permissões.", Toast.LENGTH_LONG).show();
+            else if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                QrScanner();
+        }
+    }
 }
