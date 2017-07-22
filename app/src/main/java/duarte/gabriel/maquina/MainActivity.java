4742b03892d51ffce88032c9f5f5d14c1eb9a5d9
Gabriel Duarte <gabriellagoa10@yahoo.com.br>
2017-07-22 07:23:39 -0300

    Design das telas

--- app/src/main/java/duarte/gabriel/maquina/MainActivity.java
+++ app/src/main/java/duarte/gabriel/maquina/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
