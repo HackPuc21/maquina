09d91f5e7feeeb3ac9e397c4b937bfe4abd57bac
Gabriel Duarte <gabriellagoa10@yahoo.com.br>
2017-07-21 21:45:07 -0300

    QR code

--- app/src/main/java/duarte/gabriel/maquina/MainActivity.java
+++ app/src/main/java/duarte/gabriel/maquina/MainActivity.java
@@ -1,13 +1,69 @@
 package duarte.gabriel.maquina;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import com.google.zxing.Result;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    Button btnAtivarLeitor;
+    final int CODE = 0;
+    private ZXingScannerView mScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAtivarLeitor = (Button)findViewById(R.id.btnTeste);
+
+        btnAtivarLeitor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                QrScanner(v);
+            }
+        });
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    public void QrScanner(View view){
+
+
+        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+        setContentView(mScannerView);
+
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();         // Start camera
+
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+
+        Log.e("handler", rawResult.getText()); // Prints scan results
+        Log.e("handler", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode)
+
+        // show the scanner result into dialog box.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(rawResult.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
+
+        // If you would like to resume scanning, call this method below:
+        // mScannerView.resumeCameraPreview(this);
+    }
+
+
 }
