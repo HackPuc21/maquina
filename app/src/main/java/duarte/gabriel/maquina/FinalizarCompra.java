1011eb8c4d77b2178643a1d72b76123f2403d542
Gabriel Duarte <gabriellagoa10@yahoo.com.br>
2017-07-22 13:48:44 -0300

    Visualizador de itens na tela de finalizar

--- app/src/main/java/duarte/gabriel/maquina/FinalizarCompra.java
+++ app/src/main/java/duarte/gabriel/maquina/FinalizarCompra.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
@@ -29,6 +30,7 @@ public class FinalizarCompra extends AppCompatActivity {
     private ArrayList<HashMap<String, String>> listProducts;
     OrderManager orderManager;
     Order order;
+    TextView txtValue, txtQuantidade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +40,10 @@ public class FinalizarCompra extends AppCompatActivity {
         products = (HashMap<String, Integer>) intent.getSerializableExtra("products");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        txtValue = (TextView)findViewById(R.id.valorFinal);
+        txtQuantidade = (TextView)findViewById(R.id.txtQuantidade);
+
         list =(ListView)findViewById(R.id.lista);
         build();
         create();
@@ -45,7 +51,7 @@ public class FinalizarCompra extends AppCompatActivity {
 
     private void create(){
         listProducts = new ArrayList<HashMap<String,String>>();
-
+        int total = 0;
         HashMap<String,String> temp=new HashMap<String, String>();
         temp.put("First", "Nome");
         temp.put("Second", "Quant.");
@@ -59,12 +65,16 @@ public class FinalizarCompra extends AppCompatActivity {
             temp.put("Third", "R$ 0,00");
             temp.put("Fourth", "R$ 0,00");
             listProducts.add(temp);
+            total += p.getValue();
         }
 
 
         AdapterListView adapter=new AdapterListView(this, listProducts);
         list.setAdapter(adapter);
-
+        if(total == 1)
+            txtQuantidade.setText(total+" item.");
+        else
+            txtQuantidade.setText(total+" itens.");
     }
 
     @Override
@@ -77,8 +87,7 @@ public class FinalizarCompra extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
         if(item.getItemId() == android.R.id.home){
-            startActivity(new Intent(this, MainActivity.class));
-            this.finish();
+            goBack();
             return true;
         }
         if (item.getItemId() == R.id.action_finalizar) {
@@ -96,7 +105,7 @@ public class FinalizarCompra extends AppCompatActivity {
             @Override
             public void onServiceBound() {
                 // Service is bound
-                order = orderManager.createDraftOrder("ORDER_REFERENCE");
+                order = orderManager.createDraftOrder("PEDIDO");
             }
 
             @Override
@@ -113,16 +122,12 @@ public class FinalizarCompra extends AppCompatActivity {
             String sku = "2891820317391823";
             String name = "Coca-cola lata";
 
-// Preço unitário em centavos
             int unitPrice = 550;
             int quantity = 3;
 
-// Unidade de medida do produto String
-            String unityOfMeasure = "UNIDADE";
+            order.addItem(sku, name, unitPrice, quantity, "UNIDADE");
 
-            order.addItem(sku, name, unitPrice, quantity, unityOfMeasure);
             orderManager.placeOrder(order);
-
             PaymentListener paymentListener = new PaymentListener() {
                 @Override
                 public void onStart() {
@@ -132,21 +137,31 @@ public class FinalizarCompra extends AppCompatActivity {
                 @Override
                 public void onPayment(@NotNull Order order) {
                     Log.d("MinhaApp", "Um pagamento foi realizado.");
+                    Toast.makeText(FinalizarCompra.this, "Pagamento realizado!", Toast.LENGTH_LONG).show();
+                    goBack();
                 }
 
                 @Override public void onCancel() {
                     Log.d("MinhaApp", "A operação foi cancelada.");
+                    Toast.makeText(FinalizarCompra.this, "Pagamento não efetuado.", Toast.LENGTH_LONG).show();
                 }
 
                 @Override public void onError(@NotNull PaymentError paymentError) {
                     Log.d("MinhaApp", "Houve um erro no pagamento.");
+                    Toast.makeText(FinalizarCompra.this, "Pagamento não efetuado.", Toast.LENGTH_LONG).show();
                 }
             };
 
             String orderId = order.getId();
             long value = order.getPrice();
+            txtValue.setText("R$ " + value/100 + "," + value%100);
 
             orderManager.checkoutOrder(orderId, value, paymentListener);
         }
+    }
+
+    private void goBack(){
+        startActivity(new Intent(this, MainActivity.class));
+        this.finish();
     }
 }
